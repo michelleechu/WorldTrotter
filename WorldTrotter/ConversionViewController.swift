2f913a8574b57cf9f8bf9889f542157798e1c2c8
Michelle Chu <michellechu@Michelles-MacBook-Pro-2.local>
2018-08-05 13:57:05 -0700

    first commit

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Michelle Chu on 7/31/18.
+//  Copyright © 2018 bnr. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    @IBOutlet var celsiusLabel: UILabel!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = "\(celsiusValue.value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+}
